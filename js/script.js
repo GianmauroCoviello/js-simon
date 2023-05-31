5a013bd1c3efb47aff0bc1858b6dd9e6719eb1f4
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-05-31 17:59:16 +0200

    [DOCS] Esercizio completato.

--- js/script.js
+++ js/script.js
@@ -1,28 +1,22 @@
 
 let randomNumbers =[]
+// let numeri = randomNumbers.length
 
 for ( i = 1; i < 5 +1; i++) {
     
     let numeri = parseInt(Math.floor((Math.random() * 100)))
 
     randomNumbers.push(numeri)
+   
     
     
 
     
 }
-
 console.log(randomNumbers)
 
 
-
-    
-
-
-
-
-document.getElementById('numbers').
-innerHTML = randomNumbers
+document.getElementById('numbers').innerHTML = randomNumbers
 
 second = 3
 
@@ -30,7 +24,7 @@ second = 3
 
 setTimeout(function () {
     document.getElementById('numbers').innerHTML = ''
-    document.getElementById('question').innerHTML = 'inserisci i numeri precendi'
+    document.getElementById('question').innerHTML = 'inserisci i numeri precedenti'
 
     
     
@@ -45,20 +39,28 @@ setTimeout(function() {
     let numbersProm = []
     
     for ( i = 0; i < 5; i++) {
-    let answer = parseInt(prompt('inserisci il numero visto precedentemente'))
-
-    numbersProm.push(answer)
+        let answer = parseInt(prompt('inserisci il numero visto precedentemente'))
+        numbersProm.push(answer)
                     
     }
-    if (numbersProm.includes(randomNumbers[4])) {
 
-        console.log('hai indovinato almeno un numero')
-        
-    }else{
-        console.log('non hai indovinato nulla')
+    let guessedNumber = []
+    let score = 0
+    for (let i = 0; i < randomNumbers.length; i++) {
+        if (numbersProm.includes(randomNumbers[i])) {
+            guessNumber.push(randomNumbers[i])
+            score++
+
+            document.getElementById('result').innerHTML = `hai indovinato ${score}, i numeri corretti sono ${guessedNumber}  `
     
+            
+        
+        } else{
+            document.getElementById('result').innerHTML = 'non ne hai indovinato nemmeno uno'
+        }
+        
+        
     }
-       
     
 },6000)
 
--- readme.md
+++ readme.md
@@ -1,1 +1,12 @@
 PROBLEMA: Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi. Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt(). Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+1- inserire una variabile che conterrà un array vuoto
+2- inserire un ciclo for che inserira nell' array 5 numeri random tramite l'ausilio di Mathrandom e Mathfloor e tramite la proprieta push inserisco i valori nell'array vuoto
+3- inserire un setTimeout di 5 secondi che fara scomparire l' array di numeri random
+4- inserire un ulteriore setTimeout di una durata di 6 secondi dove al suo interno:
+    4.1- inserire una variabile che conterrà un array vuoto
+    4.2- inserire un ciclo for che inserira nell' array 5 comandi promt dove l'utente inserira cinque numeri e tramite la proprieta push inserisco i valori nell'array vuoto
+5- inserire una condizione:
+    SE- i numeri inseriti nel prompt dall'utente sono inclusi anche nell'array dei numeri random allora verranno immessi nell'DOM sottoforma di numero/i indovinato/i
+    ALTRIMENTI- se i numeri inseriti nel prompt dall'utente sono esclusi dall'array dei numeri random comparira un messaggio che indicherà all'utente di non aver indovinato nemmeno un numero.
+6-FINE
